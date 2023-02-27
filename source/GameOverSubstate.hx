4e97d4969d2e3da2114625a37ce9ca71b37cf5fc
Random people <98900217+justaguy6@users.noreply.github.com>
2023-02-27 18:10:47 -0300

    Update GameOverSubstate.hx

--- source/GameOverSubstate.hx
+++ source/GameOverSubstate.hx
@@ -73,6 +73,8 @@ class GameOverSubstate extends MusicBeatSubstate
 		FlxG.camera.target = null;
 
 		boyfriend.playAnim('firstDeath');
+		
+		addVirtualPad(NONE, A_B);
 
 		camFollowPos = new FlxObject(0, 0, 1, 1);
 		camFollowPos.setPosition(FlxG.camera.scroll.x + (FlxG.camera.width / 2), FlxG.camera.scroll.y + (FlxG.camera.height / 2));
