6c84f70ebf63823311089f57cfa3928a1925e1f8
Random people <98900217+justaguy6@users.noreply.github.com>
2023-03-01 14:28:15 -0300

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -134,7 +134,7 @@ class Main extends Sprite
 
 		errMsg += "\nUncaught Error: " + e.error + "\nPlease report this error to the GitHub page: https://github.com/ShadowMario/FNF-PsychEngine\n\n> Crash Handler written by: sqirra-rng";
 
-		if (! FileSystem.exists(Main.path + "./crash/"))
+		if (!FileSystem.exists(Main.path + "./crash/"))
 			FileSystem.createDirectory(Main.path + "./crash/");
 
 		File.saveContent(path, errMsg + "\n");
