f49206062268c38cd51da4bc33aeec439a7c2853
Random people <98900217+justaguy6@users.noreply.github.com>
2023-03-06 11:28:05 -0300

    Update CustomFadeTransition.hx

--- source/CustomFadeTransition.hx
+++ source/CustomFadeTransition.hx
@@ -1,5 +1,4 @@
 package;
-
 import Conductor.BPMChangeEvent;
 import flixel.FlxG;
 import flixel.addons.ui.FlxUIState;
@@ -19,80 +18,64 @@ class CustomFadeTransition extends MusicBeatSubstate {
 	private var leTween:FlxTween = null;
 	public static var nextCamera:FlxCamera;
 	var isTransIn:Bool = false;
-	var transWhite:FlxSprite;
+	var transBlack:FlxSprite;
 	var transGradient:FlxSprite;
 
-	public function new(duration:Float, isTransIn:Bool, sound:Bool = false) {
+	public function new(duration:Float, isTransIn:Bool) {
 		super();
 
 		this.isTransIn = isTransIn;
 		var zoom:Float = CoolUtil.boundTo(FlxG.camera.zoom, 0.05, 1);
 		var width:Int = Std.int(FlxG.width / zoom);
 		var height:Int = Std.int(FlxG.height / zoom);
-		// transGradient = FlxGradient.createGradientFlxSprite(width, height, (isTransIn ? [0x0, FlxColor.BLACK] : [FlxColor.BLACK, 0x0]));
-		// transGradient.scrollFactor.set();
-		// add(transGradient);
+		transGradient = FlxGradient.createGradientFlxSprite(width, height, (isTransIn ? [0x0, FlxColor.BLACK] : [FlxColor.BLACK, 0x0]));
+		transGradient.scrollFactor.set();
+		add(transGradient);
 
-		transWhite = new FlxSprite().makeGraphic(width, height + 400, FlxColor.WHITE);
-		transWhite.scrollFactor.set();
-		add(transWhite);
+		transBlack = new FlxSprite().makeGraphic(width, height + 400, FlxColor.BLACK);
+		transBlack.scrollFactor.set();
+		add(transBlack);
 
-		// transGradient.x -= (width - FlxG.width) / 2;
-		// transWhite.x = transGradient.x;
+		transGradient.x -= (width - FlxG.width) / 2;
+		transBlack.x = transGradient.x;
 
 		if(isTransIn) {
-			transWhite.alpha = 1;
-			FlxTween.tween(transWhite, {alpha: 0}, duration, {
+			transGradient.y = transBlack.y - transBlack.height;
+			FlxTween.tween(transGradient, {y: transGradient.height + 50}, duration, {
 				onComplete: function(twn:FlxTween) {
 					close();
-				}
-			});
-			// transGradient.y = transWhite.y - transWhite.height;
-			// FlxTween.tween(transGradient, {y: transGradient.height + 50}, duration, {
-			// 	onComplete: function(twn:FlxTween) {
-			// 		close();
-			// 	},
-			// ease: FlxEase.linear});
+				},
+			ease: FlxEase.linear});
 		} else {
-			transWhite.alpha = 0;
-			FlxTween.tween(transWhite, {alpha: 1}, duration, {
+			transGradient.y = -transGradient.height;
+			transBlack.y = transGradient.y - transBlack.height + 50;
+			leTween = FlxTween.tween(transGradient, {y: transGradient.height + 50}, duration, {
 				onComplete: function(twn:FlxTween) {
 					if(finishCallback != null) {
 						finishCallback();
 					}
-				}
-			});
-			if(sound)
-				FlxG.sound.play(Paths.sound("sparkle"));
-			// transGradient.y = -transGradient.height;
-			// transWhite.y = transGradient.y - transWhite.height + 50;
-			// leTween = FlxTween.tween(transGradient, {y: transGradient.height + 50}, duration, {
-			// 	onComplete: function(twn:FlxTween) {
-			// 		if(finishCallback != null) {
-			// 			finishCallback();
-			// 		}
-			// 	},
-			// ease: FlxEase.linear});
+				},
+			ease: FlxEase.linear});
 		}
 
 		if(nextCamera != null) {
-			transWhite.cameras = [nextCamera];
-			// transGradient.cameras = [nextCamera];
+			transBlack.cameras = [nextCamera];
+			transGradient.cameras = [nextCamera];
 		}
 		nextCamera = null;
 	}
 
 	override function update(elapsed:Float) {
 		if(isTransIn) {
-			// transWhite.y = transGradient.y + transGradient.height;
+			transBlack.y = transGradient.y + transGradient.height;
 		} else {
-			// transWhite.y = transGradient.y - transWhite.height;
+			transBlack.y = transGradient.y - transBlack.height;
 		}
 		super.update(elapsed);
 		if(isTransIn) {
-		// 	transWhite.y = transGradient.y + transGradient.height;
+			transBlack.y = transGradient.y + transGradient.height;
 		} else {
-		// 	transWhite.y = transGradient.y - transWhite.height;
+			transBlack.y = transGradient.y - transBlack.height;
 		}
 	}
 
