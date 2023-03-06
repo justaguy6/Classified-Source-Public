010763e3beb5c65e95f7feded6d30cbb53a65fe6
Random people <98900217+justaguy6@users.noreply.github.com>
2023-03-06 17:18:34 -0300

    Update CustomFadeTransition.hx

--- source/CustomFadeTransition.hx
+++ source/CustomFadeTransition.hx
@@ -18,64 +18,80 @@ class CustomFadeTransition extends MusicBeatSubstate {
 	private var leTween:FlxTween = null;
 	public static var nextCamera:FlxCamera;
 	var isTransIn:Bool = false;
-	var transBlack:FlxSprite;
+	var transWhite:FlxSprite;
 	var transGradient:FlxSprite;
 
-	public function new(duration:Float, isTransIn:Bool) {
+	public function new(duration:Float, isTransIn:Bool, sound:Bool = false) {
 		super();
 
 		this.isTransIn = isTransIn;
 		var zoom:Float = CoolUtil.boundTo(FlxG.camera.zoom, 0.05, 1);
 		var width:Int = Std.int(FlxG.width / zoom);
 		var height:Int = Std.int(FlxG.height / zoom);
-		transGradient = FlxGradient.createGradientFlxSprite(width, height, (isTransIn ? [0x0, FlxColor.BLACK] : [FlxColor.BLACK, 0x0]));
-		transGradient.scrollFactor.set();
-		add(transGradient);
+		// transGradient = FlxGradient.createGradientFlxSprite(width, height, (isTransIn ? [0x0, FlxColor.BLACK] : [FlxColor.BLACK, 0x0]));
+		// transGradient.scrollFactor.set();
+		// add(transGradient);
 
-		transBlack = new FlxSprite().makeGraphic(width, height + 400, FlxColor.BLACK);
-		transBlack.scrollFactor.set();
-		add(transBlack);
+		transWhite = new FlxSprite().makeGraphic(width, height + 400, FlxColor.WHITE);
+		transWhite.scrollFactor.set();
+		add(transWhite);
 
-		transGradient.x -= (width - FlxG.width) / 2;
-		transBlack.x = transGradient.x;
+		// transGradient.x -= (width - FlxG.width) / 2;
+		// transWhite.x = transGradient.x;
 
 		if(isTransIn) {
-			transGradient.y = transBlack.y - transBlack.height;
-			FlxTween.tween(transGradient, {y: transGradient.height + 50}, duration, {
+			transWhite.alpha = 1;
+			FlxTween.tween(transWhite, {alpha: 0}, duration, {
 				onComplete: function(twn:FlxTween) {
 					close();
-				},
-			ease: FlxEase.linear});
+				}
+			});
+			// transGradient.y = transWhite.y - transWhite.height;
+			// FlxTween.tween(transGradient, {y: transGradient.height + 50}, duration, {
+			// 	onComplete: function(twn:FlxTween) {
+			// 		close();
+			// 	},
+			// ease: FlxEase.linear});
 		} else {
-			transGradient.y = -transGradient.height;
-			transBlack.y = transGradient.y - transBlack.height + 50;
-			leTween = FlxTween.tween(transGradient, {y: transGradient.height + 50}, duration, {
+			transWhite.alpha = 0;
+			FlxTween.tween(transWhite, {alpha: 1}, duration, {
 				onComplete: function(twn:FlxTween) {
 					if(finishCallback != null) {
 						finishCallback();
 					}
-				},
-			ease: FlxEase.linear});
+				}
+			});
+			if(sound)
+				FlxG.sound.play(Paths.sound("sparkle"));
+			// transGradient.y = -transGradient.height;
+			// transWhite.y = transGradient.y - transWhite.height + 50;
+			// leTween = FlxTween.tween(transGradient, {y: transGradient.height + 50}, duration, {
+			// 	onComplete: function(twn:FlxTween) {
+			// 		if(finishCallback != null) {
+			// 			finishCallback();
+			// 		}
+			// 	},
+			// ease: FlxEase.linear});
 		}
 
 		if(nextCamera != null) {
-			transBlack.cameras = [nextCamera];
-			transGradient.cameras = [nextCamera];
+			transWhite.cameras = [nextCamera];
+			// transGradient.cameras = [nextCamera];
 		}
 		nextCamera = null;
 	}
 
 	override function update(elapsed:Float) {
 		if(isTransIn) {
-			transBlack.y = transGradient.y + transGradient.height;
+			// transWhite.y = transGradient.y + transGradient.height;
 		} else {
-			transBlack.y = transGradient.y - transBlack.height;
+			// transWhite.y = transGradient.y - transWhite.height;
 		}
 		super.update(elapsed);
 		if(isTransIn) {
-			transBlack.y = transGradient.y + transGradient.height;
+		// 	transWhite.y = transGradient.y + transGradient.height;
 		} else {
-			transBlack.y = transGradient.y - transBlack.height;
+		// 	transWhite.y = transGradient.y - transWhite.height;
 		}
 	}
 
