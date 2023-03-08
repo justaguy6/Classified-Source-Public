2a8de62a7bf79cab6fa56fcdce94899e27f625bb
Sirox <71220271+Sirox228@users.noreply.github.com>
2023-03-08 08:31:19 +0300

    format and remove useless

--- source/CamFuckShader.hx
+++ source/CamFuckShader.hx
@@ -13,16 +13,15 @@ class CamFuckShader extends FlxShader
     #pragma header
 
     uniform sampler2D bgImage;
-    uniform vec3 iResolution;
 
     void main()
     {        
         vec2 uv = openfl_TextureCoordv;
-        vec2 uv2= openfl_TextureCoordv;
-        uv *= 1.5;
-        uv += vec2(-.25, -.25);
+        vec2 uv2 = openfl_TextureCoordv;
+        uv *= vec2(1.5);
+        uv += vec2(-.25);
         vec3 col = texture2D(bitmap, uv).rgb;
-        if(uv.x < 0.|| uv.x > 1.|| uv.y < 0.|| uv.y > 1.)
+        if(uv.x < 0. || uv.x > 1. || uv.y < 0. || uv.y > 1.)
             col = texture2D(bgImage, uv2).rgb;
         gl_FragColor = vec4(col,1.0);
     }
