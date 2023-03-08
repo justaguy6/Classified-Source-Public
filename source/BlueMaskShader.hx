6da613c957bd7b6a9a6c34f3d95498d2fa2259ac
Sirox <71220271+Sirox228@users.noreply.github.com>
2023-03-08 08:28:22 +0300

    bruh

--- source/BlueMaskShader.hx
+++ source/BlueMaskShader.hx
@@ -20,10 +20,10 @@ class BlueMaskShader extends FlxShader
     	vec2 uv = openfl_TextureCoordv;
 
     	vec4 base = flixel_texture2D(bitmap, uv);
-      vec4 mask = flixel_texture2D(mask, uv);
-    	float alpha = 1.0 - (mask.b * mask.a);
+      vec4 masktex = flixel_texture2D(mask, uv);
+    	float alpha = 1.0 - (masktex.b * masktex.a);
 
-    	gl_FragColor = vec4(base.rgb*alpha*base.a, alpha*base.a);
+    	gl_FragColor = vec4(base.rgb*vec3(alpha)*vec3(base.a), alpha*base.a);
     }
 
   ')
