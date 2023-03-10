de93680a3f69da83939ba30ec039b05c5f7bcef5
Random people <98900217+justaguy6@users.noreply.github.com>
2023-03-10 14:31:23 -0300

    Update WateryShader.hx

--- source/WateryShader.hx
+++ source/WateryShader.hx
@@ -12,7 +12,7 @@ class WateryShader extends FlxBasic
 
     public function new():Void{
         super();
-        shader.distortTexture.input = Assets.getBitmapData(Paths.getPreloadPath('images/heatwave.png'));
+        shader.distortTexture.input = BitmapData.fromFile(Paths.getPreloadPath('images/heatwave.png'));
     }
 
     override public function update(elapsed:Float):Void{
