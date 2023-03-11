03ff8ae09f19db91e1a091483eb942f602c00f00
Random people <98900217+justaguy6@users.noreply.github.com>
2023-03-11 06:42:34 -0300

    Update WateryShader.hx

--- source/WateryShader.hx
+++ source/WateryShader.hx
@@ -21,6 +21,28 @@ class WateryShader extends FlxBasic
         shader.iTime.value = [iTime];
     }
 }
+package;
+
+import openfl.display.BitmapData;
+import flixel.FlxBasic;
+import flixel.system.FlxAssets.FlxShader;
+
+class WateryShader extends FlxBasic
+{
+    public var shader(default, null):FabsShaderGLSL = new FabsShaderGLSL();
+    var iTime:Float = 0;
+
+    public function new():Void{
+        super();
+        shader.distortTexture.input = BitmapData.fromFile(Paths.getPreloadPath('images/heatwave.png'));
+    }
+
+    override public function update(elapsed:Float):Void{
+        super.update(elapsed);
+        iTime += elapsed;
+        shader.iTime.value = [iTime];
+    }
+}
 
 class FabsShaderGLSL extends FlxShader
 {
@@ -42,12 +64,12 @@ class FabsShaderGLSL extends FlxShader
                 vec4 dst_map_val = flixel_texture2D(distortTexture, p_d);
 
                 vec2 dst_offset = dst_map_val.xy;
-                dst_offset -= vec2(.5,5.);
+                dst_offset -= vec2(.5,.5);
                 dst_offset *= 2.;
                 dst_offset *= 0.0135; //THIS CONTROLS THE INTENSITY [higher numbers = MORE WAVY]
 
                 //reduce effect towards Y top
-                dst_offset *= pow(p_m.t, 1.4); //THIS CONTROLS HOW HIGH UP THE SCREEN THE EFFECT GOES [higher numbers = less screen space]
+                // dst_offset *= pow(p_m.t, 1.4); //THIS CONTROLS HOW HIGH UP THE SCREEN THE EFFECT GOES [higher numbers = less screen space]
 
                 vec2 dist_tex_coord = p_m.st + dst_offset;
                 gl_FragColor = flixel_texture2D(bitmap, dist_tex_coord); 
